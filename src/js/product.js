90d7fbd03d5ac36cbad45cf64f918a0686256c2b
Kelsey-Hansel <kelseylhansel@gmail.com>
2026-03-19 15:15:05 -0500

    Week 2 team activity code progress.

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,8 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-
-function addProductToCart(product) {
-  const existingCart = getLocalStorage("so-cart") || [];
-  existingCart.push(product);
-  setLocalStorage("so-cart", existingCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+const product = new ProductDetails(productId, dataSource);
+product.init();
